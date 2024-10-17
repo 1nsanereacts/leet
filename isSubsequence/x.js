ec31750be33944a9a5a5a0b8f7e638769fb74755
21 questions . <150579399+pawan-Pediredla@users.noreply.github.com>
2024-10-17 13:54:30 +0530

    x.js

---
+++ isSubsequence/x.js
@@ -0,0 +1,14 @@
+var isSubsequence= function(s,t){
+  let sp = 0;
+  let tp = 0;
+  while( sp < sp.length && tp < tp.length){
+    if(s[sp] === t[tp]){
+      sp++;
+      
+    }
+    tp++
+    
+  }
+  return s=== sp.length;
+  
+}
