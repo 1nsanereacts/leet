fc2a49295c8310ddee5297035d70d54c2ba51992
pavan . <150579399+1nsanereacts@users.noreply.github.com>
2024-10-21 10:43:04 +0530

    Reverse

---
+++ reverseinteger/x.z.a.js
@@ -0,0 +1,20 @@
+class Solution{
+  reverse(x){
+    const MIN = -2147483648
+    
+      const MAX= 2147483647
+    let res=0
+    
+    while(x!==0){
+      
+      const digit= x%10
+      x= Math.trunc(x/10)
+      if() return 0
+      if() return 0 
+      res= res*10+ digit 
+      
+      
+    }
+    return res
+  }
+}
